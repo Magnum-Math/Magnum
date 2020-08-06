09efecf65f936e114a918f8e7aa0f8c31e132600
Mayur Selukar <mrselukar@gmail.com>
2020-08-06 16:14:54 +0000

    touching up the exec

--- Training_Experiments.py
+++ Training_Experiments.py
@@ -26,6 +26,7 @@ while selection not in ["yes", "no"]:
 
 apiWrapper.getUsrQues(qry)
 RAW_TEXT, Query = apiWrapper.callApi()
+Query = Query.replace("|","")
 print("Query Received is ", Query)
 print("Solution Generated")
 
@@ -59,14 +60,14 @@ def read_file(path_to_file):
 
 
 # Construct GPT object and show some examples
-gpt = GPT(engine="curie",
-          temperature=0.3,
+gpt = GPT(engine="davinci",
+          temperature=0.01,
           max_tokens=300)
 
 
 # reade file and convert it to source string and target string tuples
-source_names = [item for item in sorted(glob("./Training_Example/sources/*"))]
-target_names = [item for item in sorted(glob("./Training_Example/latex/*"))]
+source_names = [item for item in sorted(glob("./Training_Example/text2latex/sources/*"))]
+target_names = [item for item in sorted(glob("./Training_Example/text2latex/latex/*"))]
 
 
 # open each file in the Training_Example directory
@@ -93,34 +94,80 @@ print("")
 # In[5]:
 
 
-## Testing Script
-"""
-testFiles = [item for item in sorted(glob("./Training_Example/Test/2..txt"))]
-for file in testFiles:
-    test_RAW = read_file(file)
-    response = []
-    for line in test_RAW:
-        print("Prompt: ", line)
-        t = gpt.get_top_reply(line)
-        print(t,end="----\n")
-        response.append(t)
-"""
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from api import GPT, Example, UIConfig
+from api import demo_web_app
+from glob import glob
+def read_file(path_to_file):
+    retval = ""
+    file = open(path_to_file)
+    retval = file.readlines()
+    file.close()
+    #Make sure the new line character is not read it throws the model off     
+    retval = [x.split("/n")[0][:-1] for x in retval]
+    return retval
+
+
+# Construct GPT object and show some examples
+gpt_py = GPT(engine="davinci",
+          temperature=0.01,
+          max_tokens=100)
+
+
+# reade file and convert it to source string and target string tuples
+source_names = [item for item in sorted(glob("./Training_Example/text2py/sources/*"))]
+target_names = [item for item in sorted(glob("./Training_Example/text2py/python/*"))]
+
+
+# open each file in the Training_Example directory
+for src_path, target_path in zip(source_names,target_names):
+    
+    # For each files read the RAW and corrosponding Latex Code
+    src_RAW = read_file(src_path)
+    target_RAW = read_file(target_path)
+    
+    # for each pair of RAW and latex prime the GPT model
+    if len(src_RAW) != len(target_RAW):
+        raise Exception("Source and Latex have mismached number of line {} {} in file {} and {}".format(str(len(src_RAW)), str(len(target_RAW)),src_path,target_path))
+
+    for s_RAW, t_RAW in zip(src_RAW,target_RAW):
+        gpt_py.add_example(Example(s_RAW,t_RAW))
+        # Uncomment the following if you would like to see the priming examples
+        #print("Source: ", s_RAW)
+        #print("Output: ", t_RAW)
+        #print("----")
+        
 print("")
 
 
-# In[6]:
+# In[13]:
+
+
+# Converting RAW_TEXT Query to Python Function:
+print("Attempting to convert input query to graphable python function")
+python_func = gpt_py.get_top_reply(Query)
+python_func = python_func[7:]
+print("Interpereted python function is", python_func)
+
+
+# In[17]:
 
 
 # Converting RAW_TEXT to Latex:
+from tqdm.auto import tqdm
 print("Fetching the intermediate LateX code from OpenAI GPT3 API")
 response = []
-for line in RAW_TEXT :
+for i in tqdm(range(len(RAW_TEXT))) :
+    line = RAW_TEXT[i]
     t = gpt.get_top_reply(line)
     response.append(t)
 print("Intermediate LateX generated")
 
 
-# In[7]:
+# In[18]:
 
 
 latex_code = []
@@ -132,7 +179,7 @@ for line in response:
         latex_code.append(text + "\n")
 
 
-# In[8]:
+# In[19]:
 
 
 if selection == "yes":
@@ -140,20 +187,20 @@ if selection == "yes":
         print(line, end="")
 
 
-# In[13]:
+# In[27]:
 
 
 from app import latex2Manim
-#import importlib
-#importlib.reload(latex2Manim)
+import importlib
+importlib.reload(latex2Manim)
 print("Converting Latex to Maxnim Code")
-manim_code = latex2Manim.latex2Manim(latex_code,qry, "x")
+manim_code = latex2Manim.latex2Manim(latex_code,Query ,python_func)
 if selection == "yes":
     print(manim_code)
 print("Manim Code Generated")
 
 
-# In[14]:
+# In[28]:
 
 
 fptr =  open("solution.py", "w") 
@@ -162,7 +209,7 @@ fptr.close()
 print("Manim Code saved at ./solution.py")
 
 
-# In[15]:
+# In[29]:
 
 
 #!manim solution.py Solution -pl --media_dir "./Animations"
